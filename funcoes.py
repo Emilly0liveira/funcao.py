4b72b5901e4bbf89e7fbe9294adf39c7dee3528d
Emilly Oliveira <156032999+Emilly0liveira@users.noreply.github.com>
2024-04-27 15:18:38 -0300

    Add files via upload

---
+++ funcoes.py
@@ -0,0 +1,11 @@
+def maior_entre_dois(num1, num2):
+    num1: 10
+    num2: 20
+    if num1 > num2:
+        return num1
+    else:
+        return num2
+
+# Utilizando a função para comparar os números 10 e 20
+resultado = maior_entre_dois(10, 20)
+print("O maior número entre 10 e 20 é:", resultado)
